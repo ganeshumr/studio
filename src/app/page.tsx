85238f028466e08ca3243815e94f2c2355c1360d
murali ganesh <umrgpixel@gmail.com>
2025-09-16 06:13:53 +0000

    when click this redirect to this link https://www.jaaga.ai/documents (_f

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -108,6 +108,7 @@ export default function Home() {
                 'loan-services': 'https://www.jaaga.ai/documents',
                 'land-survey': 'https://www.jaaga.ai/services/property-services/digital-land-survey',
                 'electricity-bill-updates': 'https://www.jaaga.ai/services/property-services/electricity-name-change',
+                'property-tax': 'https://www.jaaga.ai/documents',
               };
 
               const isExternalLink = Object.keys(externalLinks).includes(category.slug);
