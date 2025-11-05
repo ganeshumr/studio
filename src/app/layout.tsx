a9478872b5da7fe604143beb1ea89c37cc00546e
murali ganesh <umrgpixel@gmail.com>
2025-11-05 06:32:05 +0000

    <meta name="google-site-verification" content="DXQ8v30NfhIWCK_BWdbspTx-6

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -77,6 +77,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta name="google-site-verification" content="DXQ8v30NfhIWCK_BWdbspTx-6Jpi1TjsWvFbTytJhBI" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
