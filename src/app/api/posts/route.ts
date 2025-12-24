866980017b27018e6532804ef9240ef76821445a
murali ganesh <umrgpixel@gmail.com>
2025-12-24 05:55:08 +0000

    Try fixing this error: `Runtime Error: Error: Failed to execute 'json' o

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,6 +1,8 @@
-import { posts } from "@/lib/data";
+
+import { getPosts } from "@/lib/server/data";
 
 export async function GET(request: Request) {
+  const posts = getPosts();
   return new Response(JSON.stringify(posts), {
     status: 200,
     headers: { "Content-Type": "application/json" },
