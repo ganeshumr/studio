ccd5ec56bd97ca860b58bdbb5d1c5fd17165f0dd
murali ganesh <umrgpixel@gmail.com>
2025-12-24 08:13:35 +0000

    0:{"a":"$@1","f":"","b":"development"} 1:E{"digest":"3005775996","name":

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,6 +1,5 @@
 import {genkit} from 'genkit';
-import { googleAI } from '@genkit-ai/google-genai';
-
+import {googleAI} from '@genkit-ai/google-genai';
 
 export const ai = genkit({
   plugins: [
@@ -8,5 +7,5 @@ export const ai = genkit({
       apiKey: process.env.GOOGLE_API_KEY!,
     }),
   ],
-})
+});
 export const model = googleAI.model('gemini-1.5-flash');
